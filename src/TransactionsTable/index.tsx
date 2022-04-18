0843a26e7a6e8eee5dab0bf654e8d8c3461f99bc
Hideki <gabrielhideki@luego.com.br>
2022-04-17 22:05:03 -0300

    information blocks

---
+++ src/TransactionsTable/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react"
+import { Container } from "./styles"
+
+export function TransactionsTable() {
+    useEffect(() =>{
+        fetch('http://localhost:3000/api/transactions')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }, []);
+
+    return(
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Dev</td>
+                        <td>20/02/2011</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento</td>
+                        <td className="withdraw">- R$12.000</td>
+                        <td>Dev</td>
+                        <td>20/02/2011</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
